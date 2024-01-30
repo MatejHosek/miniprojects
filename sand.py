66cebf2f3d0d83a3292ffcc560cdd14c10557235
Matěj Hošek <matthy.hosek@gmail.com>
2024-01-30 18:03:25 +0100

    Code cleanup

--- sand.py
+++ sand.py
@@ -1,15 +1,17 @@
-import pygame
-from random import choice
-from math import sqrt
-
 # ---- SETTINGS ----
-max_fps = 30
+max_fps = 60
 screen_size = (800, 800)
 grid_size = (100, 100)
 brush_radius = 3
 
-snad_colors = ['#f6d7b0', '#f2d2a9', '#eccca2', '#e7c496', '#e1bf92'] # Palette from https://www.color-hex.com/color-palette/11392
+# Palette from https://www.color-hex.com/color-palette/11392
+snad_colors = ['#f6d7b0', '#f2d2a9', '#eccca2', '#e7c496', '#e1bf92'] 
 
+import pygame
+from random import choice
+from math import sqrt
+
+# Pygame setup
 pygame.init()
 screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
@@ -18,6 +20,19 @@ running = True
 # Sand setup
 cellDimensions = (screen_size[0] // grid_size[0], screen_size[1] // grid_size[1])
 grid = [ [None] * grid_size[1] for _ in range(grid_size[0]) ]
+
+def isInBounds(coords: list, size: list) -> bool:
+    if not hasattr(coords, '__iter__'):
+        coords = [coords]
+
+    if not hasattr(size, '__iter__'):
+        size = [size]
+
+    for i in range(len(coords)):
+        if coords[i] < 0 or coords[i] > size[i] - 1:
+            return False
+        
+    return True
 
 while running:
     # Limit FPS, prevent sand from falling too fast
@@ -37,11 +52,11 @@ while running:
         position = (pygame.mouse.get_pos()[0] // cellDimensions[0], pygame.mouse.get_pos()[1] // cellDimensions[1])
 
         for i in range(-brush_radius, brush_radius):
-            if i + position[1] < 0 or i + position[1] >= grid_size[1]:
+            if not isInBounds(i + position[1], grid_size[1]):
                 continue
             
             for j in range(-brush_radius, brush_radius):
-                if j + position[0] < 0 or j + position[0] >= grid_size[0]:
+                if not isInBounds(j + position[0], grid_size[0]):
                     continue
 
                 if sqrt(i*i + j*j) < brush_radius:
@@ -62,17 +77,17 @@ while running:
                 continue
 
             # Grains falling straight down
-            if i + 1 < grid_size[0] and grid[i + 1][j] == None:
+            if isInBounds(i + 1, grid_size[0]) and grid[i + 1][j] == None:
                 newGrid[i + 1][j] = grid[i][j]
                 continue
 
             # Grains falling to neighboring columns
             dir = choice([-1, 1])
-            if i + 1 < grid_size[0] and j + dir < grid_size[1] and j + dir >= 0 and grid[i + 1][j + dir] == None:
+            if isInBounds((i + 1, j + dir), grid_size) and grid[i + 1][j + dir] == None:
                 newGrid[i + 1][j + dir] = grid[i][j]
                 continue
 
-            if i + 1 < grid_size[0] and j - dir < grid_size[1] and j - dir >= 0 and grid[i + 1][j - dir] == None:
+            if isInBounds((i + 1, j - dir), grid_size) and grid[i + 1][j - dir] == None:
                 newGrid[i + 1][j - dir] = grid[i][j]
                 continue
 
